2d59a56f0a12aa747c3a5f2d1c5d36ca390e9306
Mark Mendoza <mendoza.mark.a@gmail.com>
2015-09-07 00:29:07 -0400

    reorder

--- Gulpfile.js
+++ Gulpfile.js
@@ -10,48 +10,31 @@ var shell = require('gulp-shell');
 var request = require('request');
 var jsdom = require('jsdom').jsdom;
 var find = require('../findAndReplaceDOMText/src/findAndReplaceDOMText.js');
+var exec = require('child_process').exec;
 
-var opts = nomnom
-.options({
-    'use-cached': {
-        abbr: 'c',
-        help: 'don\'t fetch new course information',
-        flag: true
-    }
-})
-.parse();
 gulp.task('clean-build', function(cb) {
     del(['build'], cb);
-});
-
-gulp.task('clean-dist', function(cb) {
-    del(['dist'], cb);
 });
 
 gulp.task('build-dir', ['clean-build'], function(cb) {
     mkdirp('build', cb);
 });
 
-gulp.task('dist-dir', ['clean-dist'], function(cb) {
-    mkdirp('dist', cb);
-});
-
 gulp.task('scrape-subjects',['build-dir'],
   shell.task('node src/extract-subjects.js -o build/subjects.json')
 );
 
-var extractCommand = 'node src/extract-courses.js -o build/courses.json';
-if (opts['use-cached']) {
-    gulp.task('scrape-courses', function() {
-        // no-op since we're using cached courses
-    });
-} else {
-    gulp.task('scrape-courses', ['scrape-subjects','build-dir'], shell.task(extractCommand));
-}
-
-gulp.task('inject-class-data', ['scrape-courses'],
-  shell.task('node src/inject-class-data.js -o build/shoob.html')
+gulp.task('scrape-courses', ['scrape-subjects','build-dir'],
+  shell.task('node src/extract-courses.js -o build/courses.json')
 );
+
+gulp.task('clean-dist', function(cb) {
+    del(['dist'], cb);
+});
+
+gulp.task('dist-dir', ['clean-dist'], function(cb) {
+    mkdirp('dist', cb);
+});
 
 gulp.task('vendor-js', ['dist-dir'], function() {
     return gulp.src('src/vendor/**/*.js')
@@ -65,13 +48,18 @@ gulp.task('css', ['dist-dir'], function() {
     .pipe(gulp.dest('./dist/'));
 });
 
-gulp.task('js', ['dist-dir','inject-class-data'], function() {
+gulp.task('js', ['dist-dir'], function() {
     return gulp.src('./src/client/main.js')
     .pipe(browserify())
     .pipe(gulp.dest('./dist/'));
 });
 
+gulp.task('inject-class-data',
+  shell.task(' -o build/shoob.html')
+);
+
 gulp.task('generate-client', ['dist-dir', 'css', 'js', 'vendor-js'], function(cb) {
+   exec('node src/inject-class-data.js', function (err, stdout, stderr) {
     fs.readFile('./build/shoob.html', function(error, data) {
         if (error) {
             cb(error);
